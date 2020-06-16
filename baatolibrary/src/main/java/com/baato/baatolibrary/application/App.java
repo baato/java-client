39da4006ad126715a2b49945fd328aaf09e5d41b
Samyoga Bhattarai <bhattaraisamyoga@gmail.com>
2020-06-16 09:20:04 +0545

    Update base url

--- baatolibrary/src/main/java/com/baato/baatolibrary/application/App.java
+++ baatolibrary/src/main/java/com/baato/baatolibrary/application/App.java
@@ -30,7 +30,7 @@ public class App extends Application {
 
     public static Retrofit retrofitV2(String apiVersion) {
         Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl("http://baato.io/api/v" + apiVersion + "/")
+                .baseUrl("https://api.baato.io/api/v" + apiVersion + "/")
                 .addConverterFactory(GsonConverterFactory.create());
 
 
