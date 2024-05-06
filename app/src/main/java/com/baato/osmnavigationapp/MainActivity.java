877b10ecc9f715e801342a47bce38f22e8cb81be
Nabin Dhakal <nabin.dhakal@kathmandulivinglabs.org>
2024-05-06 15:35:11 +0545

    route response compatible with maplibre

--- app/src/main/java/com/baato/osmnavigationapp/MainActivity.java
+++ app/src/main/java/com/baato/osmnavigationapp/MainActivity.java
@@ -8,16 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.baato.baatolibrary.models.SearchAPIResponse;
 import com.baato.baatolibrary.models.DirectionsAPIResponse;
 import com.baato.baatolibrary.models.LatLon;
-import com.baato.baatolibrary.models.Geometry;
 
 import com.baato.baatolibrary.models.PlaceAPIResponse;
-import com.baato.baatolibrary.navigation.BaatoTranslationMap;
 import com.baato.baatolibrary.services.BaatoPlace;
 import com.baato.baatolibrary.services.BaatoRouting;
 import com.baato.baatolibrary.services.BaatoReverse;
 import com.baato.baatolibrary.services.BaatoSearch;
-import com.baato.baatolibrary.utilities.BaatoUtil;
 import com.kathmandulivinglabs.osmnavigationapp.R;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void performRouting() {
-        String points[] = new String[]{"27.73405,85.33685", "27.7177,85.3278"};
+        String points[] = new String[]{"27.73405,85.33685", "27.6777, 85.3489"};
         new BaatoRouting(this)
                 .setPoints(points)
                 .setAccessToken(Constants.TOKEN)
@@ -46,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(DirectionsAPIResponse directionResponse) {
                         // success response here
                         Log.d(TAG, "onSuccess: routes " + directionResponse.toString());
+
                     }
 
                     @Override
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .doRequest();
     }
+
+
 
     private void performReverseGeoCoding() {
         new BaatoReverse(this)
